355468adccb2e9d927c7a6c672506bcbb585a791
apurvp24 <89583272+apurvp24@users.noreply.github.com>
2021-08-26 18:27:04 +0530

    Add files via upload

---
+++ DeveloperInterviewExercise/FileData/Exceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileData
+{
+    class InvalidArgumentsException : Exception
+    {
+        public InvalidArgumentsException() { }
+
+        public InvalidArgumentsException(string args)
+            : base(String.Format("Invalid arguments {0}", args))
+        {
+
+        }
+    }
+
+    
+}
